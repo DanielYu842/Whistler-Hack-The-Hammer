6cc5274a891d2c0f63fc7821b153895efd217e1b
DanielYu2004 <DanielYu2004@users.noreply.github.com>
2019-12-14 16:02:16 -0500

    push

--- Frontend/react frontend/src/Pages/js/Feed.js
+++ Frontend/react frontend/src/Pages/js/Feed.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../css/Feed.css'
 import Post from './Post.js';
-
+import UploadFileButton from './Button.js'
 
 class Feed extends React.Component {
     constructor(){
@@ -23,8 +23,13 @@ class Feed extends React.Component {
                     <Post title="this is a larger post this is a larger post this is a larger post"></Post>
                     <Post title="this is a post "></Post>
                     <Post title="this is a post this is a post"></Post>
+                    <Post title="this is a post this is a post"></Post>
 
 
+                </div>
+
+                <div className="button-div">
+                    <UploadFileButton></UploadFileButton>
                 </div>
             </div>
         )
