fcb72d8041d3410f4e50c053f5ff37f3af64f646
DanielYu2004 <DanielYu2004@users.noreply.github.com>
2019-12-17 23:14:53 -0500

    del

--- WhistleBlowerBackend/WhistleBlowerBackend - Copy/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './components/App.js';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-
